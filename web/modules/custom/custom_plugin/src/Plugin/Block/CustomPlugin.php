68411245956b7b9be6ede750b3d2a950ae684cb2
Oleksii Chystiakov <oleksii.chistyakov@nxte.nl>
2023-03-19 21:30:19 +0200

    added custom plugin block

---
+++ web/modules/custom/custom_plugin/src/Plugin/Block/CustomPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\custom_plugin\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides an example block.
+ *
+ * @Block(
+ *   id = "custom_plugin",
+ *   admin_label = @Translation("Custom Plugin Example"),
+ *   category = @Translation("Custom Plugin")
+ * )
+ */
+class CustomPlugin extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      'tab' => [
+        '#type' => 'link',
+        '#title' => t('Go to the home page'),
+        '#url' => Url::fromRoute('<front>'),
+      ],
+    ];
+
+    return $build;
+  }
+
+}
